912273c96db0831d0c3e026df028f3f613f29777
Yoseph Berhane <yosephbet@gmail.com>
2023-02-27 22:42:28 +0100

    change the api

--- index.js
+++ index.js
@@ -1,127 +1,117 @@
-const API_KEY = "99";
-const API_URL = "https://altcademy-to-do-list-api.herokuapp.com/tasks";
+$(document).ready(function () {
+  var baseUrl = "https://jsonplaceholder.typicode.com";
 
-const getAndDisplayAllTasks = async () => {
-  try {
-    const response = await fetch(`${API_URL}?api_key=${API_KEY}`);
-    const data = await response.json();
-    const tasks = data.tasks;
-    const todoList = document.querySelector("#todo-list");
-    todoList.innerHTML = "";
-    tasks.forEach((task) => {
-      const taskEl = createTaskElement(task);
-      todoList.appendChild(taskEl);
+  var getAndDisplayAllTasks = function () {
+    $.ajax({
+      type: "GET",
+      url: baseUrl + "/todos",
+      dataType: "json",
+      success: function (response, textStatus) {
+        $("#todo-list").empty();
+        response.forEach(function (task) {
+          $("#todo-list").append(
+            '<div class="row"><p class="col-xs-8">' +
+              task.title +
+              '</p><button class="delete" data-id="' +
+              task.id +
+              '">Delete</button><input type="checkbox" class="mark-complete" data-id="' +
+              task.id +
+              '"' +
+              (task.completed ? "checked" : "") +
+              "></div>"
+          );
+        });
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
     });
-  } catch (error) {
-    console.error(error);
-  }
-};
+  };
 
-const createTask = async (content) => {
-  try {
-    await fetch(`${API_URL}?api_key=${API_KEY}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ task: { content } }),
+  var createTask = function () {
+    $.ajax({
+      type: "POST",
+      url: baseUrl + "/todos",
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        title: $("#new-task-content").val(),
+        completed: false,
+      }),
+      success: function (response, textStatus) {
+        $("#new-task-content").val("");
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
     });
-    document.querySelector("#new-task-content").value = "";
-    await getAndDisplayAllTasks();
-  } catch (error) {
-    console.error(error);
-  }
-};
+  };
 
-const deleteTask = async (id) => {
-  try {
-    await fetch(`${API_URL}/${id}?api_key=${API_KEY}`, {
-      method: "DELETE",
+  $("#create-task").on("submit", function (e) {
+    e.preventDefault();
+    createTask();
+  });
+
+  var deleteTask = function (id) {
+    $.ajax({
+      type: "DELETE",
+      url: baseUrl + "/todos/" + id,
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
     });
-    await getAndDisplayAllTasks();
-  } catch (error) {
-    console.error(error);
-  }
-};
+  };
 
-const markTaskComplete = async (id) => {
-  try {
-    await fetch(`${API_URL}/${id}/mark_complete?api_key=${API_KEY}`, {
-      method: "PUT",
+  $(document).on("click", ".delete", function () {
+    deleteTask($(this).data("id"));
+  });
+
+  var markTaskComplete = function (id) {
+    $.ajax({
+      type: "PATCH",
+      url: baseUrl + "/todos/" + id,
+      dataType: "json",
+      data: JSON.stringify({
+        completed: true,
+      }),
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
     });
-    await getAndDisplayAllTasks();
-  } catch (error) {
-    console.error(error);
-  }
-};
+  };
 
-const markTaskActive = async (id) => {
-  try {
-    await fetch(`${API_URL}/${id}/mark_active?api_key=${API_KEY}`, {
-      method: "PUT",
+  var markTaskActive = function (id) {
+    $.ajax({
+      type: "PATCH",
+      url: baseUrl + "/todos/" + id,
+      dataType: "json",
+      data: JSON.stringify({
+        completed: false,
+      }),
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
     });
-    await getAndDisplayAllTasks();
-  } catch (error) {
-    console.error(error);
-  }
-};
+  };
 
-const createTaskElement = (task) => {
-  const taskEl = document.createElement("div");
-  taskEl.className = "row";
-
-  const checkboxEl = document.createElement("input");
-  checkboxEl.type = "checkbox";
-  checkboxEl.checked = task.completed;
-  checkboxEl.addEventListener("change", async () => {
-    if (checkboxEl.checked) {
-      await markTaskComplete(task.id);
+  $(document).on("change", ".mark-complete", function () {
+    var id = $(this).data("id");
+    if (this.checked) {
+      markTaskComplete(id);
     } else {
-      await markTaskActive(task.id);
+      markTaskActive(id);
     }
   });
 
-  const contentEl = document.createElement("span");
-  contentEl.textContent = task.content;
-  contentEl.className = "content";
-
-  const deleteButtonEl = document.createElement("button");
-  deleteButtonEl.textContent = "Delete";
-  deleteButtonEl.className = "delete-button";
-  deleteButtonEl.addEventListener("click", async () => {
-    await deleteTask(task.id);
-  });
-
-  taskEl.appendChild(checkboxEl);
-  taskEl.appendChild(contentEl);
-  taskEl.appendChild(deleteButtonEl);
-
-  return taskEl;
-};
-
-document.addEventListener("DOMContentLoaded", function () {
-  const checkboxEl = document.querySelector("input[type='checkbox']");
-  if (checkboxEl) {
-    checkboxEl.addEventListener("change", function () {
-      const todoList = document.querySelector("#todo-list");
-      const completedTasks = todoList.querySelectorAll("input[type='checkbox']:checked");
-      const numCompletedTasks = completedTasks.length;
-      const numTotalTasks = todoList.querySelectorAll(".row").length;
-      const progressEl = document.querySelector("#progress");
-      progressEl.textContent = `${numCompletedTasks}/${numTotalTasks}`;
-    });
-  }
-
-  const formEl = document.querySelector("#create-task");
-  formEl.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const content = document.querySelector("#new-task-content").value;
-    if (content.trim() === "") {
-      alert("Please enter a task.");
-      return;
-    }
-    await createTask(content);
-  });
-
-  document.addEventListener("DOMContentLoaded", async () => {
-    await getAndDisplayAllTasks();
-  });
-})
+  getAndDisplayAllTasks();
+});
